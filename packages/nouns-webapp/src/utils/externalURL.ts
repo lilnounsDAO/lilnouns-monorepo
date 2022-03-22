a00cd992161cbd56a43d74a5b0c38649a98e6814
Eric <99817443+lilnounsDAO@users.noreply.github.com>
2022-03-22 23:04:58 +0000

    updated footer links and discord redirects

--- packages/nouns-webapp/src/utils/externalURL.ts
+++ packages/nouns-webapp/src/utils/externalURL.ts
@@ -8,12 +8,12 @@ export enum ExternalURL {
 export const externalURL = (externalURL: ExternalURL) => {
   switch (externalURL) {
     case ExternalURL.discord:
-      return 'http://discord.gg/nouns';
+      return 'http://discord.gg/lilnouns';
     case ExternalURL.twitter:
-      return 'https://twitter.com/nounsdao';
+      return 'https://twitter.com/lilnounsdao';
     case ExternalURL.notion:
       return 'https://nouns.notion.site/Explore-Nouns-a2a9dceeb1d54e10b9cbf3f931c2266f';
     case ExternalURL.discourse:
-      return 'https://discourse.nouns.wtf/';
+      return 'https://discourse.lilnouns.wtf/';
   }
 };
