4e122d9b9fd1389f70716c7eba63a53e74154fcd
Lil Nouns DAO <99817443+lilnounsDAO@users.noreply.github.com>
2022-05-09 08:46:14 +0100

    REACT_IS_PRELAUNCH

--- packages/nouns-webapp/src/config.ts
+++ packages/nouns-webapp/src/config.ts
@@ -78,7 +78,7 @@ const getAddresses = (): ContractAddresses => {
 
 const config = {
   app: app[CHAIN_ID],
-  isPreLaunch: process.env.REACT_APP_IS_PRELAUNCH || 'true',
+  isPreLaunch: 'true',//process.env.REACT_APP_IS_PRELAUNCH || 'true',
   addresses: getAddresses(),
 };
 
