842aff541e3c29813a81e2e83c2fb5dc9b4e06b9
Lil Nouns DAO <99817443+lilnounsDAO@users.noreply.github.com>
2022-06-04 07:50:41 +0100

    patch eslint (2 still cont.)

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,9 @@ module.exports = {
   rules: {
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-non-null-asserted-optional-chain": "off",
-    "@typescript-eslint/no-unused-vars": "off"
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off"
   },
   root: true,
   env: {
