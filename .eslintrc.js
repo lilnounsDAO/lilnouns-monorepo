a124438e14fd00aecf1260f590ca80dc9aa5c67c
Lil Nouns DAO <99817443+lilnounsDAO@users.noreply.github.com>
2022-06-04 07:25:06 +0100

    eslint patch (2 cont. 🤪)

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,9 @@ module.exports = {
     'prettier',
   ],
   rules: {
-    "@typescript-eslint/no-explicit-any": "off"
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-non-null-asserted-optional-chain": "off",
+    "@typescript-eslint/no-unused-vars": "off"
   },
   root: true,
   env: {
