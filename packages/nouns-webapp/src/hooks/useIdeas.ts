e102c3cd38ced2a730742c239cb2878ced02b700
Tobias Hale <tobias.hale@deliveroo.co.uk>
2022-06-27 20:06:51 +0100

    Update useIdeas hook for form submission and error handling

--- packages/nouns-webapp/src/hooks/useIdeas.ts
+++ packages/nouns-webapp/src/hooks/useIdeas.ts
@@ -1,20 +1,24 @@
 import { useAuth } from './useAuth';
+import { useHistory } from 'react-router-dom';
+import { useState } from 'react';
 
 const HOST = 'http://localhost:5001';
-
-interface IdeaFormData {
-  title: string;
-  tldr: string;
-  description: string;
-}
 
 interface VoteFormData {
   direction: number;
   ideaId: number;
 }
 
+type SubmitError =
+  | {
+      message: string;
+    }
+  | undefined;
+
 export const useIdeas = () => {
   const { getAuthHeader } = useAuth();
+  const history = useHistory();
+  const [error, setError] = useState(undefined as SubmitError);
 
   const voteOnIdea = async (formData: VoteFormData) => {
     try {
@@ -31,13 +35,25 @@ export const useIdeas = () => {
       if (res.status === 200) {
         return data;
       }
-    } catch (e) {
-      console.log(e);
+    } catch (e: any) {
+      const error = {
+        message: e.message || 'Failed to submit your vote!',
+      };
+      setError(error);
     }
   };
 
   // Use to submit an idea
-  const submitIdea = async (formData: IdeaFormData) => {
+  const submitIdea = async (event: React.FormEvent<HTMLFormElement>) => {
+    interface FormDataElements extends HTMLFormControlsCollection {
+      title: HTMLInputElement;
+      tldr: HTMLTextAreaElement;
+      description: HTMLTextAreaElement;
+    }
+    event.preventDefault();
+
+    const { title, tldr, description } = event.currentTarget.elements as FormDataElements;
+
     try {
       const res = await fetch(`${HOST}/ideas`, {
         method: 'POST',
@@ -45,19 +61,31 @@ export const useIdeas = () => {
           ...getAuthHeader(),
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(formData),
+        body: JSON.stringify({
+          title: title.value,
+          tldr: tldr.value,
+          description: description.value,
+        }),
       });
-      const { data } = await res.json();
+
       if (res.status === 200) {
-        // pass
+        const { data } = await res.json();
+        history.push(`/ideas/${data.id}`);
       }
-    } catch (e) {
-      console.log(e);
+    } catch (e: any) {
+      const error = {
+        message: e.message || 'Failed to submit your idea!',
+      };
+      setError(error);
     }
   };
+
+  const dismissError = () => setError(undefined);
 
   return {
     voteOnIdea,
     submitIdea,
+    error,
+    dismissError,
   };
 };
