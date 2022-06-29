cb587893fa3a0be97b875f7cf3714ec3ae27e9bf
Tobias Hale <tobias.hale@deliveroo.co.uk>
2022-06-29 18:14:43 +0100

    Fix api naming error

--- packages/nouns-api/src/controllers/ideas.ts
+++ packages/nouns-api/src/controllers/ideas.ts
@@ -50,11 +50,11 @@ class IdeasController {
 
   static voteOnIdea = async (req: Request, res: Response, next: any) => {
     try {
-      const idea = await IdeasService.voteOnIdea(req.body, req.user);
+      const vote = await IdeasService.voteOnIdea(req.body, req.user);
       res.status(200).json({
         status: true,
         message: 'Voted on idea',
-        data: idea,
+        data: vote,
       });
     } catch (e: any) {
       res.status(e.statusCode).json({
