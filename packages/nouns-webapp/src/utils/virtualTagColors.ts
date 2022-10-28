b9e86e3d4a6e2022f6f5bb55734504ef962bd8fa
Tobias Hale <tobias.hale@deliveroo.co.uk>
2022-10-28 20:54:49 +0100

    Add virtual tag colors for new tags

--- packages/nouns-webapp/src/utils/virtualTagColors.ts
+++ packages/nouns-webapp/src/utils/virtualTagColors.ts
@@ -1,3 +1,6 @@
 export const virtualTagColorMap: { [key: string]: string } = {
   NEW: 'bg-[#FCA33A] text-white',
+  DISCUSSION: 'bg-[#F65874] text-white',
+  CONSENSUS: 'bg-[#7B61FF] text-white',
+  CLOSED: 'bg-[#8C8D92] text-white',
 };
