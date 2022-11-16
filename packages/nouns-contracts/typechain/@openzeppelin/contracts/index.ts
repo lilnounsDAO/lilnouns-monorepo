6b3eb72e12142756d572b9b825e4d284320ee9dc
lilnounsDAO <lilnouns@protonmail.com>
2022-11-16 10:44:48 +0000

    fix netlify deployment bug
    - missing governance abi

--- packages/nouns-contracts/typechain/@openzeppelin/contracts/index.ts
+++ packages/nouns-contracts/typechain/@openzeppelin/contracts/index.ts
@@ -3,8 +3,6 @@
 /* eslint-disable */
 import type * as access from "./access";
 export type { access };
-import type * as interfaces from "./interfaces";
-export type { interfaces };
 import type * as proxy from "./proxy";
 export type { proxy };
 import type * as token from "./token";
