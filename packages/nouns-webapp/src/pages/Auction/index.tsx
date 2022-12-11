31637b4ed581678cec4b94d1f7c4f65b9335606e
Will Hay <willhayjr@gmail.com>
2022-12-11 13:20:54 -0800

    i cannot read from chain :(

--- packages/nouns-webapp/src/pages/Auction/index.tsx
+++ packages/nouns-webapp/src/pages/Auction/index.tsx
@@ -2,12 +2,17 @@ import Banner from '../../components/Banner';
 import Auction from '../../components/Auction';
 import Documentation from '../../components/Documentation';
 import { useAppDispatch, useAppSelector } from '../../hooks';
-import { setOnDisplayAuctionNounId, setOnDisplayAuctionStartTime } from '../../state/slices/onDisplayAuction';
+import {
+  setOnDisplayAuctionNounId,
+  setOnDisplayAuctionStartTime,
+} from '../../state/slices/onDisplayAuction';
 import { push } from 'connected-react-router';
 import { nounPath } from '../../utils/history';
 import useOnDisplayAuction from '../../wrappers/onDisplayAuction';
 import { useEffect } from 'react';
 import ProfileActivityFeed from '../../components/ProfileActivityFeed';
+import { useContractCall } from '@usedapp/core';
+import abi from '../../libs/abi/auction.json';
 
 interface AuctionPageProps {
   initialAuctionId?: number;
@@ -22,18 +27,26 @@ const AuctionPage: React.FC<AuctionPageProps> = props => {
 
   const dispatch = useAppDispatch();
 
+  const test = useContractCall({
+    abi,
+    address: '0xe6A9B92c074520de8912EaA4591db1966E2e2B92',
+    method: 'auction',
+    args: [],
+  });
+
+  console.log('test', test);
+
   useEffect(() => {
     if (!lastAuctionNounId) return;
     if (!lastAuctionStartTime) return;
 
     if (initialAuctionId !== undefined && lastAuctionStartTime !== undefined) {
-
       // handle out of bounds noun path ids
       if (initialAuctionId > lastAuctionNounId || initialAuctionId < 0) {
         dispatch(setOnDisplayAuctionNounId(lastAuctionNounId));
         dispatch(setOnDisplayAuctionStartTime(lastAuctionStartTime!));
         dispatch(push(nounPath(lastAuctionNounId)));
-      } else {       
+      } else {
         if (onDisplayAuction === undefined) {
           // handle regular noun path ids on first load
           dispatch(setOnDisplayAuctionNounId(initialAuctionId));
